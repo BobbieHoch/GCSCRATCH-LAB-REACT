77ee8851553472371eb8e12914db59da049d74ca
BobbieHoch <rwhochstadt@gmail.com>
2023-02-06 19:23:22 -0500

    weatherapi

---
+++ Unit-8-React-Day4/testing/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+import ReactModal from 'react-modal';
+import { LightSwitch } from './components/LightSwitch';
+import { TestForm } from './components/TestForm'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <TestForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ weatherapi/src/components/WeatherForecast.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+
+export function WeatherForecast() {
+ 
+    //useEffect hook-BLANK ARRAY TO RUN ONCE BLANK ARRAY AS SECOND ARGUMENT
+    //LIFECYCLE MOUNTS (STEP1) NOW HOOKS--
+    useEffect(() => {
+
+
+    } , []);
+
+
+    return (
+        <div className = "WeatherForecast">
+
+
+        </div>
+    )
+}
