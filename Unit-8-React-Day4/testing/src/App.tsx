bbec000f0bad2e32574ab9f2f424e3d4898988d2
BobbieHoch <rwhochstadt@gmail.com>
2023-02-07 21:08:43 -0500

    finish

--- Unit-8-React-Day4/testing/src/App.tsx
+++ Unit-8-React-Day4/testing/src/App.tsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import ReactModal from 'react-modal';
 import { LightSwitch } from './components/LightSwitch';
-import { TestForm } from './components/TestForm'
+import { TestForm } from './components/TestForm';
+import { TodoItem } from './components/TodoItem';
 
 function App() {
   
   return (
     <div className="App">
       <TestForm />
+      <LightSwitch />
+      <TodoItem />
     </div>
   );
 }
